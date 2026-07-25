11cb14e8d31b4cfabefdc32c07e987d80637d4c2
Mohamed Hamdy <mohamedhamdy@gmail.com>
2026-07-25 14:10:47 +0300

    Folders & Files Names Changed

---
+++ tests/ui/Specs/finance/account-recievable/customers/create-customer-corporate.spec.ts
@@ -0,0 +1,44 @@
+import { test } from '@playwright/test';
+import { ManageCustomersPage } from '../../../../Pages/finance/AR/Customers/manageCustomers';
+import { CreateCustomerCorporatePage } from '../../../../Pages/finance/AR/Customers/createCustomerCorporate';
+
+
+let manageCustomersPage!: ManageCustomersPage;
+let createCustomerCorporatePage!: CreateCustomerCorporatePage;
+
+test.setTimeout(120000);
+
+test.beforeEach(async ({ page }, testInfo) => {
+
+  manageCustomersPage = new ManageCustomersPage(page);
+  createCustomerCorporatePage = new CreateCustomerCorporatePage(page);
+
+  await page.goto('/zeta/choose-module');
+  await page.waitForLoadState('load');
+
+  console.log(`Test start: ${testInfo.title}`);
+
+  await page.locator('.loader-wrapper').waitFor({
+    state: 'hidden'
+  });
+});
+
+test.afterEach(async ({ page }, testInfo) => {
+
+  await page.goto('/zeta/choose-module');
+  await page.waitForLoadState('load');
+
+  console.log(`Test end: ${testInfo.title}`);
+});
+
+test('Verify Create Customer Corporate', async () => {
+
+  await manageCustomersPage.navigateToManageCustomers();
+
+  await manageCustomersPage.verifyNavigationToManageCustomers();
+
+  await createCustomerCorporatePage.createCustomerCorporate();
+
+  await createCustomerCorporatePage.verifyCustomerCorporateCreated();
+
+});
