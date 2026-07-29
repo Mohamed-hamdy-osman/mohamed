e575267bba815225433b0fe7229d9469d2c8477e
Mohamed Hamdy <mohamedhamdy@gmail.com>
2026-07-29 23:39:13 +0300

    view safes

--- tests/ui/Specs/finance/cash-management/safes/view-safes.spec.ts
+++ tests/ui/Specs/finance/cash-management/safes/view-safes.spec.ts
@@ -1,19 +1,17 @@
 import { test } from '@playwright/test';
+import { ManageSafesPage } from '../../../../Pages/finance/cash-management/safes/manage-safes';
 import { ViewSafesPage } from '../../../../Pages/finance/cash-management/safes/view-safes';
 
+let manageSafesPage!: ManageSafesPage;
 let viewSafesPage!: ViewSafesPage;
-
 test.setTimeout(60000);
 
 test.beforeEach(async ({ page }, testInfo) => {
-
+  manageSafesPage = new ManageSafesPage(page);
   viewSafesPage = new ViewSafesPage(page);
-
   await page.goto('/zeta/choose-module');
   await page.waitForLoadState('load');
-
   console.log(`Test start: ${testInfo.title}`);
-
   await page.locator('.loader-wrapper').waitFor({ state: 'hidden' });
 });
 
@@ -21,16 +19,15 @@ test.afterEach(async ({ page }, testInfo) => {
 
   await page.goto('/zeta/choose-module');
   await page.waitForLoadState('load');
-
   console.log(`Test end: ${testInfo.title}`);
 });
 
 test('Verify View Safes', async ({ page }) => {
 
-  await viewSafesPage.navigateToViewSafes();
+  await manageSafesPage.navigateToManageSafes();
 
   await page.locator('.loader-wrapper').waitFor({ state: 'hidden' });
 
-  await viewSafesPage.verifyViewSafes();
+  await viewSafesPage.navigateToViewSafes();
 
 });
